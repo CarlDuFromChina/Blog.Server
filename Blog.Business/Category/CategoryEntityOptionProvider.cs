2443255c42b2efc75fc0c239372ad2acd49a18ca
Karl Du <18556906294@163.com>
2022-09-08 11:18:27 +0800

    fist commit

---
+++ Blog.Business/Category/CategoryEntityOptionProvider.cs
@@ -0,0 +1,16 @@
+﻿using Sixpence.Web.Entity;
+using Sixpence.Web.Module.SysParamGroup;
+using Sixpence.ORM.EntityManager;
+using System.Collections.Generic;
+
+namespace Blog.Business.Category
+{
+    public class CategoryEntityOptionProvider : IEntityOptionProvider
+    {
+        public IEnumerable<SelectOption> GetOptions()
+        {
+            var manager = EntityManagerFactory.GetManager();
+            return manager.Query<SelectOption>($"select code AS Value, name AS Name from category");
+        }
+    }
+}
